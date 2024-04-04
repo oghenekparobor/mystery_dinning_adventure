54a713881cfb5f3a4d2ca9660e9e5548ad16b0b7
Oghenekparobor Eminokanju <robor.eminokanju@gmail.com>
2024-04-04 21:12:45 +0100

    chore: starting app [test]

--- integration_test/app_startup_checker.dart
+++ integration_test/app_startup_checker.dart
@@ -6,14 +6,18 @@ import 'package:mystery_dinning_adventure/main.dart' as mystery;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('check to see the mystery app start and select category', (widgetTester) async {
+  testWidgets('check to see the mystery app start and select category',
+      (tester) async {
     // start up the mystery dinning app
     await mystery.main();
+    await tester.pumpWidget(const mystery.MyApp());
+
+    await tester.pumpAndSettle();
+
+    await Future.delayed(const Duration(milliseconds: 3000));
 
     // confirm home screen is opened
     final floatingButton = find.byKey(const ValueKey('floating_action_button'));
     expect(floatingButton, findsOneWidget);
-
-
   });
 }
